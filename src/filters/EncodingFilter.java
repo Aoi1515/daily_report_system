dc0733b5d8129c6af674bee69c5459a68ddec0d4
Aoi <aoi.kuno77@gmail.com>
2021-01-18 00:31:55 +0900

    Add Employee index

--- src/filters/EncodingFilter.java
+++ src/filters/EncodingFilter.java
@@ -32,7 +32,6 @@ public class EncodingFilter implements Filter {
      * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
      */
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
 
@@ -44,4 +43,5 @@ public class EncodingFilter implements Filter {
      */
     public void init(FilterConfig fConfig) throws ServletException {
     }
+
 }
